e10870a3d8f0d0f53663696474e762647e86a6bf
MinJoon Joo <tuk34896@temple.edu>
2024-04-25 12:05:33 -0400

    adding 3 things

--- app/src/main/java/com/example/signupformpartdeux/MainActivity.kt
+++ app/src/main/java/com/example/signupformpartdeux/MainActivity.kt
@@ -6,13 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,14 +46,44 @@ class MainActivity : ComponentActivity() {
 fun SignUpForm(name: String, modifier: Modifier = Modifier) {
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-
+        var userName by rememberSaveable { mutableStateOf("")}
+        var userAge by rememberSaveable { mutableStateOf("")}
+        var userEmail by rememberSaveable { mutableStateOf("")}
         Text(
             text = "Sign Up Form!",
             modifier = modifier.padding(8.dp),
             fontSize = 28.sp
         )
         
-        TextField(value = "", onValueChange = {}, label = {Text("Name")})
+        TextField(
+            value = userName,
+            onValueChange = {userName = it},
+
+            label = {Text("Name")},
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        TextField(
+            value = userEmail,
+            onValueChange = {userEmail = it},
+            label = {Text("Email")},
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            )
+        )
+
+
+        TextField(
+            value = userAge,
+            onValueChange = {userAge = it},
+
+            label = {Text("Age")},
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
     }
 }
 
