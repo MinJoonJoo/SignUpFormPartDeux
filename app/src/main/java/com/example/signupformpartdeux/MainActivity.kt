87323d2c60c86baef5316d82becc2d3bbbdf5e2b
MinJoon Joo <tuk34896@temple.edu>
2024-04-25 12:20:07 -0400

    button and error msg added

--- app/src/main/java/com/example/signupformpartdeux/MainActivity.kt
+++ app/src/main/java/com/example/signupformpartdeux/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.signupformpartdeux
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,8 +50,13 @@ fun SignUpForm(name: String, modifier: Modifier = Modifier) {
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         var userName by rememberSaveable { mutableStateOf("")}
+        var userNameError by rememberSaveable {
+            mutableStateOf(false)
+        }
         var userAge by rememberSaveable { mutableStateOf("")}
         var userEmail by rememberSaveable { mutableStateOf("")}
+        val context = LocalContext.current
+
         Text(
             text = "Sign Up Form!",
             modifier = modifier.padding(8.dp),
@@ -57,7 +65,9 @@ fun SignUpForm(name: String, modifier: Modifier = Modifier) {
         
         TextField(
             value = userName,
-            onValueChange = {userName = it},
+            isError = userNameError,
+            onValueChange = {userName = it
+                            userNameError = false},
 
             label = {Text("Name")},
             keyboardOptions = KeyboardOptions(
@@ -84,6 +94,20 @@ fun SignUpForm(name: String, modifier: Modifier = Modifier) {
                 keyboardType = KeyboardType.Number
             )
         )
+
+        Button(onClick ={
+            var message :String
+            if(userName.isNotBlank())
+                message = "Thank you for signing up!"
+            else {
+                message = "Please complete the form"
+                userNameError = true
+            }
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
+        }){
+            Text("Save")
+        }
     }
 }
 
